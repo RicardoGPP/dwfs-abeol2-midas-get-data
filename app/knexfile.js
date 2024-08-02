af0316972dee5b8aa859e4c7025499eb7df5cd5b
rperandre <rperandre@padtec.com.br>
2024-08-02 15:53:36 -0300

    Database configuration.

---
+++ app/knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+    client: 'pg',
+    connection: {
+        host: 'dpg-cq7h1buehbks738vf0b0-a.oregon-postgres.render.com',
+        database: 'midasdb',
+        user: 'midasdb_user',
+        password: 'ZCgajrftFzxazyUgVMZ0AC5esaGBOE3y',
+        ssl: true
+    }
+};
